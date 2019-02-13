2ccaac258dd8eda410b5ffb9e5471dd4aa223c01
Brian Twitchell <32440651+liataun@users.noreply.github.com>
2019-02-12 23:44:38 -0500

    Added PHP files from Class Notes and added source URL for image in hello-world page.

---
+++ make_errors.php
@@ -0,0 +1,16 @@
+<?php
+$temperature = 75;
+$scale = 'F';
+$location = "Cambridge";
+
+echo 'Temperature in '.$location.': '.$temperature.' '.$scale;
+
+//require 'this-file-is-bogus.php';
+
+//echo "Hello"
+//echo "World"
+
+//echo rand(5);
+
+echo $foo;
+?>
